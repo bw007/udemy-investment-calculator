294790645c67c13740d029a557a0cd8588b1e8bb
bw007 <mr.khasanov02@gmail.com>
2025-08-22 07:47:14 +0500

    practice

---
+++ src/app/data.model.ts
@@ -0,0 +1,15 @@
+export interface DataModel {
+  initialInvestment: number,
+  duration: number,
+  expectedReturn: number,
+  annualInvestment: number
+}
+
+export interface ResultsData {
+  year: number;
+  interest: number;
+  valueEndOfYear: number;
+  annualInvestment: number;
+  totalInterest: number;
+  totalAmountInvested: number;
+}
